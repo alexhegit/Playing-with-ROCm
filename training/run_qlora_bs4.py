8362059ae70494b8496a0fda9e85642880947e40
Alex He <heye_dev@163.com>
2024-05-27 08:39:20 +0800

    Add the python script of QLoRA
    Signed-off-by: Alex He <heye_dev@163.com>

---
+++ training/run_qlora_bs4.py
@@ -0,0 +1,100 @@
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    pipeline
+)
+from peft import LoraConfig
+from trl import SFTTrainer
+
+
+# Model and tokenizer names
+base_model_name = "/DATA/NousResearch/Llama-2-7b-chat-hf/"
+new_model_name = "llama-2-7b-chat-enhanced" #You can give your own name for fine tuned model
+
+# Tokenizer
+llama_tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
+llama_tokenizer.pad_token = llama_tokenizer.eos_token
+llama_tokenizer.padding_side = "right"
+
+
+# Quantization Config
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True
+)
+
+
+base_model = AutoModelForCausalLM.from_pretrained(
+    base_model_name,
+    quantization_config=quant_config,
+    device_map="auto"
+)
+base_model.config.use_cache = False
+base_model.config.pretraining_tp = 1
+
+
+# Dataset
+data_name = "mlabonne/guanaco-llama2-1k"
+training_data = load_dataset(data_name, split="train")
+# check the data
+print(training_data.shape)
+# #11 is a QA sample in English
+print(training_data[11])
+
+
+# Training Params
+train_params = TrainingArguments(
+    output_dir="./results_modified",
+    num_train_epochs=1,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=1,
+    optim="paged_adamw_32bit",
+    save_steps=50,
+    logging_steps=50,
+    learning_rate=2e-4,
+    weight_decay=0.001,
+    fp16=False,
+    bf16=False,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True,
+    lr_scheduler_type="constant",
+    report_to="tensorboard"
+) 
+
+
+from peft import get_peft_model
+# LoRA Config
+peft_parameters = LoraConfig(
+    lora_alpha=8,
+    lora_dropout=0.1,
+    r=8,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+model = get_peft_model(base_model, peft_parameters)
+model.print_trainable_parameters()
+
+
+# Trainer with LoRA configuration
+fine_tuning = SFTTrainer(
+    model=base_model,
+    train_dataset=training_data,
+    peft_config=peft_parameters,
+    dataset_text_field="text",
+    tokenizer=llama_tokenizer,
+    args=train_params
+)
+
+# Training
+fine_tuning.train()
+
+# Save Model
+fine_tuning.model.save_pretrained(new_model_name)
